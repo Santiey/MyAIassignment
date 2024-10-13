02ae97da3dc074239a75d9b48bbbe39404765498
Santiey <santanamuchena@gmail.com>
2024-10-13 19:31:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function displayQuote(response) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "93b1062870ba3t0o0f4309f684f9efc9";
     let context =
-      "You are an inspirational quote expert and love to write short quotes. You mission is to generate a quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote";
+      "You are an inspirational quote expert and you write short quotes. You mission is to generate a quote and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote";
     let prompt = `User instructions: Generate an inspirational quote about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
