1aa4725c8067addffad532bc4c58d09984558c71
Santiey <santanamuchena@gmail.com>
2024-10-13 20:30:08 +0200

    updated

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function displayQuote(response) {
       strings: response.data.answer,
       autoStart: true,
       delay: 1,
-      cursor: "",
     });
   }
   
@@ -13,17 +12,18 @@ function displayQuote(response) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "93b1062870ba3t0o0f4309f684f9efc9";
     let context =
-      "You are an inspirational quote expert and you write short quotes. You mission is to generate a quote and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote";
+    "You are an inspirational quote expert and love to write short quotes. You mission is to generate a quote and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote.";
     let prompt = `User instructions: Generate an inspirational quote about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  
-    let poemElement = document.querySelector("#quote");
-    poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
+
+    let quoteElement = document.querySelector("#quote");
+    quoteElement.classList.remove("hidden");
+    quoteElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
   
     axios.get(apiURL).then(displayQuote);
   }
   
-  let quoteFormElement = document.querySelector("#quote-generator-form");
-  quoteFormElement.addEventListener("submit", generateQuote);
+  let quoteForm = document.querySelector("#quote-form");
+  quoteForm.addEventListener("submit", generateQuote);
 
+  
