482a26c8b39fede6d41aa4e6dac682455327b233
Santiey <santanamuchena@gmail.com>
2024-10-13 16:42:59 +0200

    commit

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 function displayQuote(response) {
-
     new Typewriter("#quote", {
-        strings: response.data.answer,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
-}
-
-
-function generateQuote(event) {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  function generateQuote(event) {
     event.preventDefault();
-    
+  
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "93b1062870ba3t0o0f4309f684f9efc9";
-    let prompt = `User Instruction: Generate a quote ${promptInput.value}`;
-    let context = "You are an inspirational quotes expert and you write short inspiring quotes. Your mission is to generate inspirational quotes in basic HTML. Make sure to follow user instructions."
+    let context =
+      "You are an inspirational quote expert and love to write short quotes. You mission is to generate a quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote";
+    let prompt = `User instructions: Generate an inspirational quote about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
+  
+    let poemElement = document.querySelector("#quote");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
+  
     axios.get(apiURL).then(displayQuote);
-}
-
-let quoteFormElement = document.querySelector("#quote-generator-form");
-quoteFormElement.addEventListener("submit", generateQuote)
+  }
+  
+  let quoteFormElement = document.querySelector("#quote-generator-form");
+  quoteFormElement.addEventListener("submit", generateQuote);
 
